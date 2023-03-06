5347468826e5288c79eec6a96c280c4c23de1cab
virlanna <virllanareis@gmail.com>
2023-03-06 14:38:31 -0300

    primeiro envio

---
+++ lista2/l2q3.py
@@ -0,0 +1,7 @@
+'''faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.'''
+
+#entrada
+
+numero = int (input('digite um numero:"))
+
+#processamento e saida
---
+++ lista2/l2q4.py
@@ -0,0 +1,7 @@
+''faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.'''
+'
+#entrada
+
+
+
+
